208ed1cfaab3bfaf120c94ae620f52d65789d5ae
Gaertner <egaertne@nrel.gov>
2018-12-17 14:48:47 -0700

    ccblade airfoil method for calculating FAST Aerodyn unsteady inputs from the polar data

--- src/ccblade/ccblade.py
+++ src/ccblade/ccblade.py
@@ -51,7 +51,7 @@ class CCAirfoil(object):
     differentiable cubic spline"""
 
 
-    def __init__(self, alpha, Re, cl, cd):
+    def __init__(self, alpha, Re, cl, cd, cm=[]):
         """Setup CCAirfoil from raw airfoil data on a grid.
 
         Parameters
@@ -74,11 +74,19 @@ class CCAirfoil(object):
         alpha = np.radians(alpha)
         self.one_Re = False
 
+        if len(cm) > 0:
+            self.use_cm = True
+        else:
+            self.use_cm = False
+
         # special case if zero or one Reynolds number (need at least two for bivariate spline)
         if len(Re) < 2:
             Re = [1e1, 1e15]
             cl = np.c_[cl, cl]
             cd = np.c_[cd, cd]
+            if self.use_cm:
+                cm = np.c_[cm, cm]
+
             self.one_Re = True
 
         kx = min(len(alpha)-1, 3)
@@ -88,6 +96,9 @@ class CCAirfoil(object):
         self.cl_spline = RectBivariateSpline(alpha, Re, cl, kx=kx, ky=ky, s=0.1)
         self.cd_spline = RectBivariateSpline(alpha, Re, cd, kx=kx, ky=ky, s=0.001)
         self.alpha     = alpha
+
+        if self.use_cm > 0:
+            self.cm_spline = RectBivariateSpline(alpha, Re, cm, kx=kx, ky=ky, s=0.0001)
 
 
     @classmethod
@@ -108,7 +119,7 @@ class CCAirfoil(object):
 
         af = Airfoil.initFromAerodynFile(aerodynFile)
         alpha, Re, cl, cd, cm = af.createDataGrid()
-        return cls(alpha, Re, cl, cd)
+        return cls(alpha, Re, cl, cd, cm=cm)
 
     
     def max_eff(self, Re):
@@ -145,7 +156,7 @@ class CCAirfoil(object):
         return Emax, alpha_Emax, cl_Emax, cd_Emax
 
 
-    def evaluate(self, alpha, Re):
+    def evaluate(self, alpha, Re, return_cm=False):
         """Get lift/drag coefficient at the specified angle of attack and Reynolds number.
 
         Parameters
@@ -172,7 +183,11 @@ class CCAirfoil(object):
         cl = self.cl_spline.ev(alpha, Re)
         cd = self.cd_spline.ev(alpha, Re)
 
-        return cl, cd
+        if self.use_cm and return_cm:
+            cm = self.cm_spline.ev(alpha, Re)
+            return cl, cd, cm
+        else:
+            return cl, cd
 
 
     def derivatives(self, alpha, Re):
@@ -194,6 +209,155 @@ class CCAirfoil(object):
         return dcl_dalpha, dcl_dRe, dcd_dalpha, dcd_dRe
 
 
+    def eval_unsteady(self, alpha, cl, cd, cm):
+        # calculate unsteady coefficients from polars for OpenFAST's Aerodyn
+
+        unsteady = {}
+
+        alpha_rad = np.radians(alpha)
+        cn = cl*np.cos(alpha_rad) + cd*np.sin(alpha_rad)
+
+        # alpha0, Cd0, Cm0
+        aoa_l = [-30.]
+        aoa_h = [30.]
+        idx_low  = np.argmin(abs(alpha-aoa_l))
+        idx_high = np.argmin(abs(alpha-aoa_h))
+
+        if max(np.abs(np.gradient(cl)))>0.:
+            unsteady['alpha0'] = np.interp(0., cl[idx_low:idx_high], alpha[idx_low:idx_high])
+            unsteady['Cd0'] = np.interp(0., cl[idx_low:idx_high], cd[idx_low:idx_high])
+            unsteady['Cm0'] = np.interp(0., cl[idx_low:idx_high], cm[idx_low:idx_high])
+        else:
+            unsteady['alpha0'] = 0.
+            unsteady['Cd0'] = cd[np.argmin(abs(alpha-0.))]
+            unsteady['Cm0'] = 0.
+
+
+        unsteady['eta_e']= 1
+        unsteady['T_f0'] = "Default"
+        unsteady['T_V0'] = "Default"
+        unsteady['T_p']  = "Default"
+        unsteady['T_VL'] = "Default"
+        unsteady['b1']   = "Default"
+        unsteady['b2']   = "Default"
+        unsteady['b5']   = "Default"
+        unsteady['A1']   = "Default"
+        unsteady['A2']   = "Default"
+        unsteady['A5']   = "Default"
+        unsteady['S1']   = 0
+        unsteady['S2']   = 0
+        unsteady['S3']   = 0
+        unsteady['S4']   = 0
+
+        def find_breakpoint(x, y, idx_low, idx_high, multi=1.):
+            lin_fit = np.interp(x[idx_low:idx_high], [x[idx_low],x[idx_high]], [y[idx_low],y[idx_high]])
+            idx_break = 0
+            lin_diff = 0
+            for i, (fit, yi) in enumerate(zip(lin_fit, y[idx_low:idx_high])):
+                if multi==0:
+                    diff_i = np.abs(yi-fit)
+                else:
+                    diff_i = multi*(yi-fit)
+                if diff_i>lin_diff:
+                    lin_diff = diff_i
+                    idx_break = i
+            idx_break += idx_low
+            return idx_break
+
+        # Cn1
+        idx_alpha0  = np.argmin(abs(alpha-unsteady['alpha0']))
+        if max(np.abs(np.gradient(cm)))>0.:
+            aoa_h = alpha[idx_alpha0]+25.
+            idx_high = np.argmin(abs(alpha-aoa_h))
+
+            cm_temp = cm[idx_low:idx_high]
+            idx_cm_min = [i for i,local_min in enumerate(np.r_[True, cm_temp[1:] < cm_temp[:-1]] & np.r_[cm_temp[:-1] < cm_temp[1:], True]) if local_min] + idx_low
+            idx_high = idx_cm_min[-1]
+            
+            idx_Cn1 = find_breakpoint(alpha, cm, idx_alpha0, idx_high)
+            unsteady['Cn1'] = cn[idx_Cn1]
+        else:
+            idx_Cn1 = np.argmin(abs(alpha-0.))
+            unsteady['Cn1'] = 0.
+
+        # Cn2
+        if max(np.abs(np.gradient(cm)))>0.:
+            aoa_l = np.mean([alpha[idx_alpha0], alpha[idx_Cn1]])-30.
+            idx_low  = np.argmin(abs(alpha-aoa_l))
+
+            cm_temp = cm[idx_low:idx_high]
+            idx_cm_min = [i for i,local_min in enumerate(np.r_[True, cm_temp[1:] < cm_temp[:-1]] & np.r_[cm_temp[:-1] < cm_temp[1:], True]) if local_min] + idx_low
+            idx_high = idx_cm_min[-1]
+            
+            idx_Cn2 = find_breakpoint(alpha, cm, idx_low, idx_alpha0, multi=0.)
+            unsteady['Cn2'] = cn[idx_Cn2]
+        else:
+            idx_Cn2 = np.argmin(abs(alpha-0.))
+            unsteady['Cn2'] = 0.
+
+        # C_nalpha
+        if max(np.abs(np.gradient(cm)))>0.:
+            # unsteady['C_nalpha'] = np.gradient(cn, alpha_rad)[idx_alpha0]
+            unsteady['C_nalpha'] = max(np.gradient(cn[idx_alpha0:idx_Cn1], alpha_rad[idx_alpha0:idx_Cn1]))
+        else:
+            unsteady['C_nalpha'] = 0.
+
+        # alpha1, alpha2
+        # finding the break point in drag as a proxy for Trailing Edge separation, f=0.7
+        # 3d stall corrections cause erroneous f calculations 
+        if max(np.abs(np.gradient(cm)))>0.:
+            aoa_l = [0.]
+            idx_low  = np.argmin(abs(alpha-aoa_l))
+            idx_alpha1 = find_breakpoint(alpha, cd, idx_low, idx_Cn1, multi=-1.)
+            unsteady['alpha1'] = alpha[idx_alpha1]
+        else:
+            idx_alpha1 = np.argmin(abs(alpha-0.))
+            unsteady['alpha1'] = 0.
+        unsteady['alpha2'] = -1.*unsteady['alpha1']
+
+
+        unsteady['St_sh']   = "Default"
+        unsteady['k0']      = 0
+        unsteady['k1']      = 0
+        unsteady['k2']      = 0
+        unsteady['k3']      = 0
+        unsteady['k1']      = 0
+        unsteady['x_cp_bar']   = "Default"
+        unsteady['UACutout']   = "Default"
+        unsteady['filtCutOff'] = "Default"
+
+        self.unsteady = unsteady
+
+        # import matplotlib.pyplot as plt
+        # fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(6., 8.), sharex=True)
+        # ax[0].plot(alpha, cn)
+        # ax[0].plot(alpha, cl, '--')
+        # ax[0].plot(unsteady['alpha0'], 0.,'o')
+        # ax[0].annotate('alpha0', (unsteady['alpha0'], 0.))
+        # ax[0].plot(alpha[idx_alpha0], cn[idx_alpha0],'o')
+        # ax[0].annotate('C_nalpha', (alpha[idx_alpha0], cn[idx_alpha0]))
+        # ax[0].plot(alpha[idx_Cn1], cn[idx_Cn1],'o')
+        # ax[0].annotate('Cn1', (alpha[idx_Cn1], cn[idx_Cn1]))
+        # ax[0].plot(alpha[idx_Cn2], cn[idx_Cn2],'o')
+        # ax[0].annotate('Cn2', (alpha[idx_Cn2], cn[idx_Cn2]))
+        # ax[0].set_ylabel('C_L')
+        # ax[0].grid(True, linestyle=':')
+
+        # ax[1].plot(alpha, cd)
+        # ax[1].set_ylabel('C_D')
+        # ax[1].grid(True, linestyle=':')
+
+        # ax[2].plot(alpha, cm)
+        # ax[2].plot(alpha[idx_Cn1], cm[idx_Cn1], 'o')
+        # ax[2].annotate('Cn1', (alpha[idx_Cn1], cm[idx_Cn1]))
+        # ax[2].plot(alpha[idx_Cn2], cm[idx_Cn2], 'o')
+        # ax[2].annotate('Cn2', (alpha[idx_Cn2], cm[idx_Cn2]))
+
+        # ax[2].set_ylabel('C_M')
+        # ax[2].set_xlabel('Angle of Attack, deg')
+        # ax[2].grid(True, linestyle=':')
+
+        # plt.show()
 
 # ------------------
 #  Main Class: CCBlade
